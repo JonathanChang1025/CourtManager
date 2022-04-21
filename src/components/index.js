5d17e677b9149f357f5f2ca056a7e36a2f506328
Jonathan Chang <jonathanchang1025@outlook.com>
2022-04-20 20:10:22 -0400

    created navigation pages

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Navigation } from "./Navigation";
+export { default as Footer } from "./Footer";
+export { default as Home } from "./Home";
+export { default as Court } from "./Court";
+export { default as CheckIn } from "./CheckIn";
+export { default as QRScan } from "./QRScan";
