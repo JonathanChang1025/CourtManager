6bbad19a1a9fadb7f06fcd6fdd6bd770096a7c35
Jonathan Chang <jonathanchang1025@outlook.com>
2022-04-29 16:39:23 -0400

    added favicon and navigation logo

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import logo from "../img/logo.png"
 
 function Navigation() {
   return (
     <div className="navigation">
       <nav className="navbar navbar-expand navbar-dark bg-dark">
         <div className="container">
-          <NavLink className="navbar-brand" to="/">
-            Minton
-          </NavLink>
+        <a class="navbar-brand" href="#">
+          <img src={logo} width="30" height="30" class="d-inline-block align-top" alt=""/>
+          &nbsp;&nbsp;
+          Court Manager
+        </a>
           <div>
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
